563895b2399b43c010d325808515265c54131a0f
Gordon Hall <gordon@gordonwritescode.com>
2015-04-14 18:16:38 -0400

    implement message, bucket, contact, rpc - need tests for message and rpc

---
+++ test/message.unit.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var expect = require('chai').expect;
+var constants = require('../lib/constants');
+var Message = require('../lib/message');
+
+describe('Message', function() {
+
+  describe('@constructor', function() {
+
+
+
+  });
+
+  describe('#serialize', function() {
+
+
+
+  });
+
+});
---
+++ test/rpc.unit.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var expect = require('chai').expect;
+var constants = require('../lib/constants');
+var RPC = require('../lib/rpc');
+
+describe('RPC', function() {
+
+  describe('@constructor', function() {
+
+
+
+  });
+
+  describe('#send', function() {
+
+
+
+  });
+
+  describe('#close', function() {
+
+
+
+  });
+
+  describe('#_handleMessage', function() {
+
+
+
+  });
+
+  describe('#_expireCalls', function() {
+
+
+
+  });
+
+});
