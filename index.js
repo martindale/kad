f1116f6b0509b32b768b377b4be45d1ebeca46f4
Gordon Hall <gordon@gordonwritescode.com>
2015-11-12 12:04:50 -0500

    bump versions, export RPC constructor

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ module.exports.Logger = require('./lib/logger');
 module.exports.Message = require('./lib/message');
 module.exports.Node = require('./lib/node');
 module.exports.Router = require('./lib/router');
+module.exports.RPC = require('./lib/rpc');
 module.exports.transports = require('./lib/transports');
 module.exports.utils = require('./lib/utils');
 module.exports.constants = require('./lib/constants');
