57128d0cd7c65e014c3df23f56cba36e7346ac32
Gordon Hall <gordon@gordonwritescode.com>
2015-12-27 11:32:23 -0500

    fix minimal example code

--- example/minimal.js
+++ example/minimal.js
@@ -8,11 +8,15 @@ var kademlia = require('..');
 
 // setup a kademlia DHT in less than 10 lines of code
 var dht = kademlia.Node({
-  transport: kademlia.transports.HTTP({
-    address: 'localhost',
-    port: 3050
-  }),
-  storage: kademlia.storage.FS(require('os').tmpdir() + '/kad-example')
+  transport: kademlia.transports.HTTP(
+    kademlia.contacts.AddressPortContact({
+      address: 'localhost',
+      port: 3050
+    })
+  ),
+  storage: kademlia.storage.FS(
+    require('os').tmpdir() + '/kad-example'
+  )
 });
 
 // party on, garth
