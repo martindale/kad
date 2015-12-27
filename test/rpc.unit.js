5bb85f796cad79b0bc73c71459342c15d907cfe2
Gordon Hall <gordon@gordonwritescode.com>
2015-12-27 11:21:42 -0500

    add coverage for hooks

--- test/rpc.unit.js
+++ test/rpc.unit.js
@@ -85,19 +85,55 @@ describe('RPC', function() {
 
   describe('#before', function() {
 
-    
+    it('should register a before hook for the given event name', function() {
+      var rpc = new FakeTransport(AddressPortContact({
+        address: '0.0.0.0', port: 8080
+      }));
+      rpc.before('serialize', function(message, next) {
+        next();
+      });
+      expect(typeof rpc._hooks.before.serialize[0]).to.be.equal('function');
+    });
 
   });
 
   describe('#after', function() {
 
-    
+    it('should register an after hook for the given event name', function() {
+      var rpc = new FakeTransport(AddressPortContact({
+        address: '0.0.0.0', port: 8080
+      }));
+      rpc.after('serialize', function(message, next) {
+        next();
+      });
+      expect(typeof rpc._hooks.after.serialize[0]).to.be.equal('function');
+    });
 
   });
 
   describe('#_trigger', function() {
 
-    
+    it('should trigger the hook stack for the given event', function(done) {
+      var rpc = new FakeTransport(AddressPortContact({
+        address: '0.0.0.0', port: 8080
+      }));
+      var hook1 = sinon.stub().callsArg(1);
+      var hook2 = sinon.stub().callsArg(1);
+      var hook3 = sinon.stub().callsArg(1);
+      var hook4 = sinon.stub().callsArg(0);
+      rpc.before('test', hook1);
+      rpc.before('test', hook2);
+      rpc.before('test', hook3);
+      rpc.after('test', hook4);
+      rpc._trigger('before:test', [{ fake: 'message' }], function() {
+        rpc._trigger('after:test');
+        expect(hook1.called).to.equal(true);
+        expect(hook2.called).to.equal(true);
+        expect(hook3.called).to.equal(true);
+        expect(hook4.called).to.equal(true);
+        done();
+      });
+    });
 
   });
 
